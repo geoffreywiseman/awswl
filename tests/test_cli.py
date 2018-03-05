bcbc0ae7e026d9847fdda4f844e6227f26cd3671
Geoffrey Wiseman <geoffrey.wiseman@codiform.com>
2018-03-04 22:46:10 -0500

    Fixing Fixture Param Shadows Fixture Name
    Pytest 3 has an option to specify a fixture name, avoiding the shadowing problem that you get without that.
    
    Signed-off-by: Geoffrey Wiseman <geoffrey.wiseman@codiform.com>

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,30 +4,27 @@ import pytest
 from awswl import cli
 
 
-@pytest.fixture
-def envsgid():
+@pytest.fixture(name='sgid')
+def fixture_env_sgid():
     sgid = "sg-12345"
     os.environ[cli.AWSWL_SGID_KEY] = sgid
     yield sgid
     os.environ.unsetenv(cli.AWSWL_SGID_KEY)
 
 
-# noinspection PyShadowingNames
 def test_parse_empty_arguments():
     options = cli.parse_args([])
     assert options.ssh_port == 22
     assert options.sgid is None
 
 
-# noinspection PyShadowingNames
-def test_parse_environment_into_arguments(envsgid):
+def test_parse_environment_into_arguments(sgid):
     options = cli.parse_args([])
-    assert options.sgid == envsgid
+    assert options.sgid == sgid
 
 
-# noinspection PyShadowingNames
-def test_parse_override_defaults(envsgid):
+def test_parse_override_defaults(sgid):
     override = 'sg-67890'
     options = cli.parse_args(['--sgid', override])
     assert options.sgid == override
-    assert options.sgid != envsgid
+    assert options.sgid != sgid
